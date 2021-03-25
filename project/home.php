f0258b8a00c935f1ac929d3526440f855c75aaab
Indiedude78 <saj39@njit.edu>
2021-03-24 20:00:37 -0400

    Revamped the website UI

--- project/home.php
+++ project/home.php
@@ -6,13 +6,11 @@ $username = "";
 if (is_logged_in()) {
     $username = get_username();
     $email = get_email();
-    if ($username != NULL) {
-        echo "<h3>Welcome, $username</h3>";
-    }
-    else {
-        echo "<h3>Welcome, $email</h3>";
-    }
 }
-
 ?>
+<h3>Hello<br> <?php if ($username != NULL) {echo $username;} else {echo $email;} ?></h3><br>
+<?php
+if (!is_logged_in()) {
+    echo '<h3>Please <a href="register.php">register</a> or <a href="login.php">login</a> to continue</h3>';
+}
 
