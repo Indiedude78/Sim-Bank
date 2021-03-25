08a9e438cfa80c001b37d589557c4cd5ee202bae
Indiedude78 <saj39@njit.edu>
2021-03-25 14:41:34 -0400

    Fixing login bug

--- project/login.php
+++ project/login.php
@@ -1,8 +1,8 @@
 <?php require_once(__DIR__ . "/partials/nav.php"); ?>
 
 <form class="user-reg" method="POST">
-    <label for="email">Email:</label>
-    <input type="email" id="email" name="email"/>
+    <label for="user-login">Email or Username:</label>
+    <input type="text" id="email" name="user-login"/>
     <label for="p1">Password:</label>
     <input type="password" id="p1" name="password" required/>
     <input type="submit" name="login" value="Login"/>
@@ -12,9 +12,10 @@
 if (isset($_POST["login"])) {
     $email = null;
     $password = null;
-    if ((isset($_POST["email"]))) {
-        $email = $_POST["email"];
+    if ((isset($_POST["user-login"]))) {
+        $email = $_POST["user-login"];
     }
+
     if (isset($_POST["password"])) {
         $password = $_POST["password"];
     }
@@ -22,14 +23,14 @@ if (isset($_POST["login"])) {
     if ((!isset($email)) || !isset($password)) {
         $isValid = false;
     }
-    if (!strpos($email, "@") and (!isset($username))) {
-        $isValid = false;
-        echo "<br>Invalid email<br>";
-    }
+   // if (!strpos($email, "@")) {
+     //   $isValid = false;
+  //      echo "<br>Invalid email<br>";
+   // }
     if ($isValid) {
         $db = getDB();
         if (isset($db)) {
-            $stmt = $db->prepare("SELECT id, username, email, password from Users WHERE email = :email LIMIT 1");
+            $stmt = $db->prepare("SELECT * from Users WHERE email = :email OR username = :email");
 
             $params = array(":email" => $email);
             $r = $stmt->execute($params);
