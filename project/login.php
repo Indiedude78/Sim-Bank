df06c7e46e439e25a24498d3ce70fee078d31ad9
Indiedude78 <saj39@njit.edu>
2021-03-26 14:07:09 -0400

    User login added - M1_F2

--- project/login.php
+++ project/login.php
@@ -1,5 +1,6 @@
+<!--Include navigation bar-->
 <?php require_once(__DIR__ . "/partials/nav.php"); ?>
-
+<!--User login form-->
 <form class="user-reg" method="POST">
     <label for="user-login">Email or Username:</label>
     <input type="text" id="email" name="user-login"/>
@@ -9,18 +10,18 @@
 </form>
 
 <?php
-if (isset($_POST["login"])) {
+if (isset($_POST["login"])) { //checl to see if form is set
     $email = null;
     $password = null;
-    if ((isset($_POST["user-login"]))) {
+    if ((isset($_POST["user-login"]))) { //check to see if user-login creds are set
         $email = $_POST["user-login"];
     }
 
-    if (isset($_POST["password"])) {
+    if (isset($_POST["password"])) { //check to see if user password is set
         $password = $_POST["password"];
     }
     $isValid = true;
-    if ((!isset($email)) || !isset($password)) {
+    if ((!isset($email)) || !isset($password)) { //Fail validation if user-login creds are not set
         $isValid = false;
     }
    // if (!strpos($email, "@")) {
@@ -28,40 +29,40 @@ if (isset($_POST["login"])) {
   //      echo "<br>Invalid email<br>";
    // }
     if ($isValid) {
-        $db = getDB();
+        $db = getDB(); //get DB if validation is successful
         if (isset($db)) {
-            $stmt = $db->prepare("SELECT * from Users WHERE email = :email OR username = :email");
+            $stmt = $db->prepare("SELECT * from Users WHERE email = :email OR username = :email"); //sanitize data using placeholders
 
-            $params = array(":email" => $email);
+            $params = array(":email" => $email); //map email to its variable
             $r = $stmt->execute($params);
             //echo "db returned: " . var_export($r, true);
             $e = $stmt->errorInfo();
-            if ($e[0] != "00000") {
-                echo "uh oh something went wrong: " . var_export($e, true);
+            if ($e[0] != "00000") { //If error exists, output error message
+                echo "uh oh something went wrong";
             }
             $result = $stmt->fetch(PDO::FETCH_ASSOC);
-            if ($result && isset($result["password"])) {
+            if ($result && isset($result["password"])) { //check to see if userlogin data matches table data
                 $password_hash_from_db = $result["password"];
-                if (password_verify($password, $password_hash_from_db)) {
+                if (password_verify($password, $password_hash_from_db)) { //verify password
                     $stmt = $db->prepare("
 SELECT Roles.name FROM Roles JOIN UserRoles on Roles.id = UserRoles.role_id where UserRoles.user_id = :user_id and Roles.is_active = 1 and UserRoles.is_active = 1");
                     $stmt->execute([":user_id" => $result["id"]]);
                     $roles = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-                    unset($result["password"]);//remove password so we don't leak it beyond this page
-                    //let's create a session for our user based on the other data we pulled from the table
-                    $_SESSION["user"] = $result;//we can save the entire result array since we removed password
+                    unset($result["password"]);//remove password to prevent leak beyond this page
+                    //create a session for the user based on the other data pulled from the table
+                    $_SESSION["user"] = $result;//save the entire result array since password is removed
                     if ($roles) {
-                        $_SESSION["user"]["roles"] = $roles;
+                        $_SESSION["user"]["roles"] = $roles; //set roles if exists
                     }
                     else {
                         $_SESSION["user"]["roles"] = [];
                     }
-                    //on successful login let's serve-side redirect the user to the home page.
-                    header("Location: home.php");
+                    //on successful login let's serve-side redirect the user to the home page with a second delay.
+                    die(header("refresh:1; url: home.php"));
                 }
                 else {
-                    echo "<br>Invalid password, please try again.<br>";
+                    echo "<br>Invalid password, please try again.<br>"; //display error
                 }
             }
             else {
