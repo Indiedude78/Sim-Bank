3a943edf10c188e97015559d43f42e0550b2671c
Indiedude78 <saj39@njit.edu>
2021-04-03 13:04:21 -0400

    Additonal fixes for security

--- project/home.php
+++ project/home.php
@@ -11,6 +11,7 @@ if (is_logged_in()) { //Secuirty check to see if user is logged in
 <h3>Hello<br> <?php if ($username != NULL) {echo $username;} else {echo $email;} ?></h3><br>
 <?php
 if (!is_logged_in()) { //If user is not logged in, the can login from the home page
-    echo '<h3>Please <a href="register.php">register</a> or <a href="login.php">login</a> to continue</h3>';
+    die(header("Location: index.php"));
 }
+?>
 
--- project/index.php
+++ project/index.php
@@ -1,1 +1,8 @@
-<?php header("Location: home.php"); ?>
+<?php require_once(__DIR__ . "/partials/nav.php"); ?>
+<?php 
+if (is_logged_in()) {
+    header("Location: home.php");
+}
+?>
+
+<h2>Welcome, please <a href="register.php">register</a> or <a href="login.php">login</a></h2>
