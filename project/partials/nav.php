d5b63102d046ec16667aeee63f237bc27fc78761
Indiedude78 <saj39@njit.edu>
2021-04-07 13:56:17 -0400

    Added admin role with basic test pages

--- project/partials/nav.php
+++ project/partials/nav.php
@@ -12,7 +12,12 @@ require_once(__DIR__ . "/../lib/helpers.php");
         <?php endif;?>
         <?php if(is_logged_in()):?>
         <li><a href="profile.php">Profile</a></li>
+            <?php if (has_role("Admin")): ?>
+            <li><a href="test_create_accounts.php">Create Account</a></li>
+            <li><a href="test_edit_account.php">Edit Account</a></li>
+            <?php endif ?>
         <li><a href="logout.php">Logout</a></li>
         <?php endif; ?>
+        
     </ul>
 </nav>
