ec9603d1c4f1626b66da2a4b1337b5e1fc1d840e
Indiedude78 <saj39@njit.edu>
2021-04-14 16:06:32 -0400

    Added account creation with randomized account number

--- project/lib/helpers.php
+++ project/lib/helpers.php
@@ -62,4 +62,12 @@ function get_messages() {
     }
     return array();
 }
+
+function set_account_num($length) {
+    $result = '';
+    for ($i = 0; $i < $length; $i++) {
+        $result .= mt_rand(0, 9);
+    }
+    return $result;
+}
 ?>
