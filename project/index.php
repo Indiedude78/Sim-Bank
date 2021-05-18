323fff0747f0d5345b279277d72444b6ed693a9b
Indiedude78 <saj39@njit.edu>
2021-05-17 21:20:46 -0400

    Added GET API - part 2

--- project/index.php
+++ project/index.php
@@ -1,8 +1,25 @@
 <?php require_once(__DIR__ . "/partials/nav.php"); ?>
-<?php 
+<?php
 if (is_logged_in()) {
     header("Location: home.php");
 }
 ?>
+<div id="image-slider">
+    <ul class="slides">
+        <li class="image"> <img src="static/images/pennies_jar.jpg" alt="Savings go a long way"> </li>
+        <li class="image"> <img src="static/images/online_banking.jpg" alt="Bank online anytime"> </li>
+        <li class="image"> <img src="static/images/penny_plant.jpg" alt="Let your money grow"> </li>
+        <li class="image"> <img src="static/images/house_loan.jpg" alt="Let your dreams come true"> </li>
+    </ul>
+</div>
 
-<h2>Welcome, please <a href="register.php">register</a> or <a href="login.php">login</a></h2>
+<div id="welcome-text">
+    <h2>
+        Welcome, please
+        <span class="welcome-links"><a href="register.php">register</a></span> or
+        <span class="welcome-links"><a href="login.php">login</a></span>
+    </h2>
+</div>
+<script src="jquery/jquery.js"></script>
+
+<script src="static/js/imageSlider.js"></script>
--- project/partials/nav.php
+++ project/partials/nav.php
@@ -1,27 +1,30 @@
-<link rel="stylesheet" href="static/css/style.css">
+<link rel="stylesheet" href="static/css/updatedStyle.css">
 <?php
 //we'll be including this on most/all pages so it's a good place to include anything else we want on those pages
 require_once(__DIR__ . "/../lib/helpers.php");
 ?>
-<nav id="nav">
+<nav id="nav-main">
     <ul class="nav">
-        <li><a href="home.php">Home</a></li>
-        <?php if (!is_logged_in()) : ?>
-            <li><a href="login.php">Login</a></li>
-            <li><a href="register.php">Register</a></li>
-        <?php endif; ?>
-        <?php if (is_logged_in()) : ?>
-            <li><a href="profile.php">Profile</a></li>
-            <?php if (has_role("Admin")) : ?>
-                <li><a href="test_admin_search.php">Admin Search</a></li>
-                <li><a href="test_admin_list_users.php">Admin List</a></li>
+        <div id="nav-list">
+            <li><a href="home.php">Home</a></li>
+            <?php if (!is_logged_in()) : ?>
+                <li><a href="login.php">Login</a></li>
+                <li><a href="register.php">Register</a></li>
             <?php endif; ?>
-            <li><a href="logout.php">Logout</a></li>
+            <?php if (is_logged_in()) : ?>
+                <li><a href="profile.php">Profile</a></li>
+                <?php if (has_role("Admin")) : ?>
+                    <li><a href="test_admin_search.php">Admin Search</a></li>
+                    <li><a href="test_admin_list_users.php">Admin List</a></li>
+                <?php endif; ?>
+                <li><a href="logout.php">Logout</a></li>
+            <?php endif; ?>
+        </div>
+        <?php if (is_logged_in()) : ?>
+            <div id="nav-user">
+                <h4><?php safer_echo(get_username()); ?></h4>
+            </div>
         <?php endif; ?>
 
     </ul>
-    <!--
-    <h4><?php //safer_echo(get_username()); 
-        ?></h4>
-    -->
 </nav>
